c3cd4707df823dcab3146887a678b9f7c2e9a785
刘洪洪 <2498601771@qq.com>
2023-02-16 18:05:36 +0800

    fix: 顶部topBar添加侧边栏展开收起功能

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    isCollapse: false
   },
   getters: {
   },
   mutations: {
+    setCollapse (state, flag) {
+      state.isCollapse = flag
+    }
   },
   actions: {
   },
--- src/views/layouts/TabsBar/Index.vue
+++ src/views/layouts/TabsBar/Index.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="top-tabs-bar">
-
+    <el-radio-group v-model="isCollapse" @change="changeBtn" style="margin-bottom: 20px">
+      <el-radio-button :label="false">展开</el-radio-button>
+      <el-radio-button :label="true">关闭</el-radio-button>
+    </el-radio-group>
   </div>
 </template>
+
+<script setup>
+import {ref} from "vue";
+import {useStore} from "vuex";
+const store = useStore()
+
+const isCollapse = ref(true)
+
+const changeBtn = () => {
+  console.log('状态', isCollapse.value)
+  store.commit('setCollapse', isCollapse.value)
+}
+</script>
 
 <script>
 export default {
