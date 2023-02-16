64bf37670c6a2dc91d240e46024276bf16c4ee20
刘洪洪 <2498601771@qq.com>
2023-02-16 18:01:23 +0800

    fix: 修改入口页面框架

--- src/views/layouts/Index.vue
+++ src/views/layouts/Index.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-    <side-bar></side-bar>
-    <div class="vab-main">
-      <nav-bar></nav-bar>
-      <tabs-bar></tabs-bar>
-      <app-main></app-main>
+
+    <div class="common-layout">
+      <el-container>
+        <el-aside width="200px" class="side-bar">
+          <side-bar></side-bar>
+        </el-aside>
+        <el-container>
+          <el-main :class="store.state.isCollapse?'main-margin-collapse':'main-margin'">
+            <tabs-bar></tabs-bar>
+            <nav-bar></nav-bar>
+            <app-main></app-main>
+          </el-main>
+        </el-container>
+      </el-container>
     </div>
   </div>
 </template>
 
+<script setup>
+import {useStore} from "vuex";
+import {ref, watch} from "vue";
+const store = useStore()
+let isCollapse = ref(store.state.isCollapse)
+
+watch(() => store.state.isCollapse, (newVal) => {
+  isCollapse.value = store.state.isCollapse
+})
+</script>
 <script>
 import SideBar from '@/views/layouts/SideBar/Index'
 import NavBar from '@/views/layouts/NavBar/Index'
@@ -27,9 +46,12 @@ export default {
 </script>
 
 <style scoped>
-.vab-main {
-  min-height: calc(100vh - 2px);
-  height: 100%;
-  margin-left: 256px;
+.main-margin {
+  margin-left: 0;
+  transition: width 1s;
+}
+.main-margin-collapse {
+  margin-left: -136px;
+  transition: width 0.3s;
 }
 </style>
